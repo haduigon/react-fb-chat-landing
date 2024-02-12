9663515bd4c08909ff0924fe9324dbb21684cc2b
haduigon <haduigon@gmail.com>
2024-02-12 10:14:36 +0200

    added some styles

--- src/components/download/DownloadReport.tsx
+++ src/components/download/DownloadReport.tsx
@@ -3,24 +3,25 @@ import './DownloadReport.scss';
 import { StateContext } from '../../context/AppContext';
 import download from 'js-file-download';
 import { client } from '../../helpers/utils';
+import { GiSagittarius } from "react-icons/gi";
 
 export const DownloadReport = () => {
+
+  const sagittarius = <GiSagittarius size={30} />
+
   const { state } = useContext(StateContext);
   const report = state.forecast.length > 0 ? state.forecast : localStorage.getItem('report');
 
   const fileName = 'test.pdf';
-  async function getFile() {
 
+  async function getFile() {
     const response = await downloadFile();
-    
-    const content = response.headers['content-type']
-    console.log(content, 'content');
-    
-    download(response.data, fileName, content)
- 
+    const content = response.headers['content-type'];
+    download(response.data, fileName, content);
   }
- 
+
   async function downloadFile() {
+
     return client.get("https://ro.sms.destiny4you.com/getfile", {
       headers: {
         'content-type': '*'
@@ -30,12 +31,12 @@ export const DownloadReport = () => {
       },
       responseType: 'blob',
     })
-
   }
   return (
-    <div>
-      <div className="download-button" onClick={getFile}>Download</div>
-      <div className="report-box custom-font">
+    <div className="report-page-container">
+      <div className="sign-box">{sagittarius}</div>
+      <div className="download-button" onClick={getFile}>Download report</div>
+      <div className="report-box">
         {report}
       </div>
     </div>
